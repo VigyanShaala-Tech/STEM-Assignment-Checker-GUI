4c37b102e43abddf0484bfb725b72f532059892a
Akshata Satpute <140546785+akshatasatpute@users.noreply.github.com>
2024-06-27 15:50:52 +0530

    Update New_build.py

--- scripts/New_build.py
+++ scripts/New_build.py
@@ -1,19 +1,16 @@
-#import necessary libraries
 import streamlit as st
 import pandas as pd
-#import pyperclip  # Import the pyperclip module for clipboard operations
+import pyperclip
 import os
-from supabase_py import create_client,Client
-# Read the category dataset and extract unique categories
 import requests
-from io import StringIO  # Import StringIO directly from the io module
-from io import BytesIO
 
+from supabase_py import create_client,Client
+from io import StringIO  # Import StringIO directly from the io module
 
 # Set initial scale for very small screens
 st.markdown('<meta name="viewport" content="width=device-width, initial-scale=0.5">', unsafe_allow_html=True)
 
-
+# Display the PNG image in the top left corner of the Streamlit sidebar with custom dimensions
 # Display the PNG image in the top left corner of the Streamlit sidebar with custom dimensions
 image_path = r"C:\Users\User\Desktop\Career-Exploration-main\graphics\VS-logo.png"
 st.sidebar.image(image_path, width=150)
@@ -21,8 +18,6 @@ st.sidebar.image(image_path, width=150)
 logo_path = r"C:\Users\User\Downloads\pngtree-vector-assignment-icon-png-image_4254076.jpg"
 st.sidebar.image(logo_path, width=90)
 
-# Update the user name list with Shalini, Titli, and Deepika
-#user_names = ['Shalini', 'Titli', 'Deepika']
 # Predefined dictionary of user names and access codes known only to administrators
 user_access_codes = {
     "Shalini": "abc123",
@@ -32,7 +27,7 @@ user_access_codes = {
 }
 
 # Prompt the user to enter their access code
-entered_code = st.sidebar.text_input("Enter Your Access Code:", type="password")
+entered_code = st.sidebar.text_input("Enter Your Access Code *:", type="password")
 # Check if either of the boxes is not selected
 if not entered_code:
     st.error("Please fill in all the compulsory fields marked with * before proceeding.")
@@ -42,15 +37,14 @@ if not entered_code:
 filtered_user_names = [user_name for user_name, access_code in user_access_codes.items() if entered_code == access_code]
 
 # If a user name is found for the entered access code, display the select box for that user
-
 selected_user_name = st.sidebar.selectbox('Select Your User Name:', filtered_user_names)
 
-selected_Cohort = st.sidebar.selectbox("Select a Cohort", ["Incubator_1","Incubator_2","Incubator_3"])
+selected_Cohort = st.sidebar.selectbox("Select an option", ["Incubator_1","Incubator_2","Incubator_3"])
 
 # Display the selected option
-st.write("Selected Cohort:", selected_Cohort)
+st.write("Selected Option:", selected_Cohort)
 
-# Function to read all CSV files from a folder and store them in a dictionary
+
 # Function to read CSV files from a folder path obtained from GitHub and store them in a dictionary
 def read_assignment_files_from_github(folder_path_url):
     response = requests.get(folder_path_url)  # Get folder contents from GitHub
@@ -76,12 +70,13 @@ def read_assignment_files_from_github(folder_path_url):
         st.write("Failed to retrieve files from the GitHub repository.")
     
     return file_mapping
+
 # Function to get the dataset for a selected assignment file
 def get_dataset(selected_assignment_file):
     return file_mapping[selected_assignment_file]
 
 # Define the GitHub API URL for the folder containing CSV files
-github_folder_url = 'https://api.github.com/repos/VigyanShaala-Tech/STEM-Assignment-Checker-GUI/contents/data_files'
+github_folder_url = 'https://api.github.com/repos/akshatasatpute/Stemcheck/contents/Files'
 
 # Read all assignment files from the folder path obtained from GitHub
 file_mapping = read_assignment_files_from_github(github_folder_url)
@@ -109,6 +104,10 @@ if selected_assignment_file is not None:
     #st.write(selected_dataset)
 
 
+# Read the category dataset and extract unique categories
+import requests
+import pandas as pd
+from io import BytesIO
 
 # URL pointing to the CSV file
 file_url = 'https://bvvaailuzioczysisnoc.supabase.co/storage/v1/object/sign/Stemcheck/Comments_sheet.csv?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJTdGVtY2hlY2svQ29tbWVudHNfc2hlZXQuY3N2IiwiaWF0IjoxNzE3NjYwMzQ1LCJleHAiOjE3NDkxOTYzNDV9.gUNII7Hhc3yqRNJKJ780GVlfvOTEAi9fhxIh9AWbGV0&t=2024-06-06T07%3A52%3A24.268Z'
@@ -127,24 +126,26 @@ except Exception as e:
     print("An error occurred while reading the CSV file:", e)
 
 
+#category_dataset = pd.read_csv("C:/Users/User/Downloads/Comments sheet.csv", encoding='latin1')
 unique_categories = category_dataset['Category '].unique()
-unique_status=category_dataset['Accepted /Rejected'].unique()
+unique_status = category_dataset['Accepted /Rejected'].unique()
 
 data = get_dataset(selected_assignment_file)
+
 
 # Create a dropdown to select the file status
 file_statuses = data['status'].unique()
 selected_status = st.sidebar.selectbox('Select File Status', file_statuses)
 
+
 # Filter the data based on the selected status from the selected assignment file
 filtered_data = get_dataset(selected_assignment_file)
 filtered_data = filtered_data[filtered_data['status'] == selected_status]
 
-
 # Create a dropdown to select the email ID
 if 'user/email' in filtered_data.columns:
     email_list = filtered_data['user/email'].str.split('-').str[1].tolist()  # Extract email IDs after the hyphen
-
+    
     # Placeholder to store the list of processed email IDs
 
     if "processed_emails" not in st.session_state:
@@ -154,8 +155,9 @@ if 'user/email' in filtered_data.columns:
     email_list = [email for email in email_list if email not in st.session_state.processed_emails]
     
     selected_email = st.selectbox('Select Email ID:', email_list)
+
     # Filter data based on selected email
-    filtered_email_data = filtered_data[filtered_data['user/email'] == 'vigyanshaalainternational1617-'+selected_email]
+    filtered_email_data = filtered_data[filtered_data['user/email'] == 'vigyanshaalainternational1617-' + selected_email]
 
     # Add a copy button to copy the email address to the clipboard
     if selected_email:
@@ -165,14 +167,11 @@ if 'user/email' in filtered_data.columns:
             st.code(selected_email)
             st.write("Email address copied to clipboard")  # Inform the user that the email address has been copied
 
-        
-
     if not filtered_email_data.empty:
-       latest_submission_email = None
-       latest_submission_col = None
-       latest_submission_no = None
-    
-    
+        latest_submission_email = None
+        latest_submission_col = None
+        latest_submission_no = None
+
     # Get the latest filled column without NA or blank for the file name format 'data/{i}/fileName'
     for col in filtered_email_data.columns[::-1]:  # Iterate in reverse to get the last filled column
         if 'fileName' in col:
@@ -181,29 +180,21 @@ if 'user/email' in filtered_data.columns:
                 latest_submission_col = col
                 break
 
-    #if latest_submission_email is not None:
-        # Display the latest file name in the Streamlit sidebar
-        #st.sidebar.markdown(f"File name: {latest_submission_email} (from column: {latest_submission_col})")
-        #st.markdown(f"File name: {latest_submission_email}")
-        #st.markdown(f"Submission No:{latest_submission_col}")
     file_name = latest_submission_col
     split_file_name = file_name.split('/')
     if len(split_file_name) > 2:
         result = split_file_name[1]  # Extract the part between the slashes
-        latest_submission_no=result
+        latest_submission_no = result
     else:
         print("Invalid format")
-    
+
     if latest_submission_email is not None:
-        #st.markdown(f"File name: {latest_submission_email} (Submission No: {latest_submission_col})")
-        #st.markdown(f"File name: {latest_submission_email} (Submission No: {result})")
         st.markdown(f"File name: {latest_submission_email}")
     else:
         st.sidebar.text("No valid file name found")
 
-
     if latest_submission_no is not None:
-        st.markdown(f"Submission Number:{latest_submission_no}")
+        st.markdown(f"Submission Number: {latest_submission_no}")
 
     if not filtered_email_data.empty:
         latest_messages = []
@@ -224,12 +215,10 @@ if 'user/email' in filtered_data.columns:
     else:
         st.write('No data found for the selected email')
 
-    
 
 # Display the select boxes with asterisk for compulsory selection
 selected_category = st.sidebar.selectbox('Select category*', unique_categories, key='category_select')
 selected_category_status = st.sidebar.radio('Select comments category status*', unique_status, key='category_status_radio')
-
 
 # Check if either of the boxes is not selected
 if not selected_category or not selected_category_status:
@@ -237,24 +226,23 @@ if not selected_category or not selected_category_status:
     st.stop()
 
 
+
 # Filter the comments based on acceptance or rejection
 if 'Accepted /Rejected' in category_dataset.columns and 'Comment' in category_dataset.columns:
-    selected_category_accepted = category_dataset[(category_dataset['Category '] == selected_category) & (category_dataset['Accepted /Rejected'] ==selected_category_status)]['Comment'].tolist()
+    selected_category_accepted = category_dataset[(category_dataset['Category '] == selected_category) & (category_dataset['Accepted /Rejected'] == selected_category_status)]['Comment'].tolist()
     
     if selected_category_accepted:
         # Create a multiselect to choose from the available comments
-        selected_comments_accepted = st.multiselect('Select Comments:', selected_category_accepted)
-
-
+        selected_comments_accepted = st.multiselect('Select Comments:*', selected_category_accepted)
+        
         # Check if either of the boxes is not selected
         if not selected_comments_accepted:
             st.error("Please fill in all the compulsory fields marked with * before proceeding.")
             st.stop()
-            
+
         if selected_comments_accepted:
             selected_comments_text_accepted = '\n'.join(selected_comments_accepted)
             comment_area_accepted = st.text_area('Selected Comments:', value=selected_comments_text_accepted, height=120)
-
 
         # Text area to allow the user to provide a custom comment
         custom_comment = st.text_area('Add Custom Comment:', height=60)
@@ -265,13 +253,12 @@ if 'Accepted /Rejected' in category_dataset.columns and 'Comment' in category_da
         if all_comments:
             selected_comments_text_accepted = '\n'.join(all_comments)
 
-
-
 # Create a text box to enter marks for the selected email ID and assignment file
 if selected_email and selected_assignment_file:
     marks_key = f"marks_{selected_email}_{selected_assignment_file}"
     entered_marks = st.text_input("Enter Marks (Integer only):", key=marks_key)
     marks = int(entered_marks) if entered_marks.isdigit() else None if entered_marks else None
+
 
     # Check if either of the boxes is not selected
     if not marks:
@@ -288,6 +275,7 @@ if selected_email and selected_assignment_file:
 st.write("")
 
 unique_key = latest_submission_email + " " + f"_Email {selected_email}"+" "+f"_Sub No:{latest_submission_no}"
+
 # Define a function to create a DataFrame with the provided data
 def create_feedback_dataframe(unique_key,selected_user_name,selected_assignment_file, selected_status, latest_submission_email, latest_submission_no, selected_email, latest_messages, selected_comments_accepted,marks,selected_Cohort):
     data = {
@@ -307,9 +295,11 @@ def create_feedback_dataframe(unique_key,selected_user_name,selected_assignment_
     feedback_df = pd.DataFrame(data)
     return feedback_df
 
+
 url: str = 'https://bvvaailuzioczysisnoc.supabase.co'
 key: str = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2dmFhaWx1emlvY3p5c2lzbm9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc1OTEwNTMsImV4cCI6MjAzMzE2NzA1M30.kDC8CsjhjB_XkVo9OM3EchNMWk5ytyc-s629I0-qr1k'
 supabase: Client = create_client(url, key)
+
 
 # Function to save feedback data to Supabase
 def save_feedback_to_supabase(feedback_data, table_name):
@@ -403,6 +393,4 @@ if all_comments:
         
 # Add the processed email to the session state to remove it from the dropdown
         st.session_state.processed_emails.append(selected_email)
-
-
 
